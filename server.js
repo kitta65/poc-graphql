02a5c420762e30289fa9a8bd47323ea861a244c5
kitta65 <26474260+kitta65@users.noreply.github.com>
2025-01-03 15:05:03 +0900

    generate schema automatically

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const { createHandler } = require("graphql-http/lib/use/express");
-const { buildSchema } = require("graphql");
 const { ruruHTML } = require("ruru/server");
+const {
+  GraphQLObjectType,
+  GraphQLInputObjectType,
+  GraphQLNonNull,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+} = require("graphql");
 
 let numVisitors = 0;
 const visitors = [];
@@ -24,49 +32,91 @@ class Calculator {
   }
 }
 
-const schema = buildSchema(`
-  input VisitorInput {
-    id: ID!
-  }
-  type Query {
-    dummy: String!
-    format(in_: String!): String!
-    getCalculator(left: Int!): Calculator!
-    getNumVisitors: Int!
-  }
-  type Calculator {
-    add(right: Int!): Int!
-    sub(right: Int!): Int!
-  }
-  type Mutation {
-    incrementNumVisitors(visitor: VisitorInput!): Int!
-  }`);
+const visitorInputType = new GraphQLInputObjectType({
+  name: "VisitorInput",
+  fields: {
+    id: { type: new GraphQLNonNull(GraphQLID) },
+  },
+});
 
-const rootValue = {
-  dummy() {
-    return "this is dummy response!";
+const calculatorType = new GraphQLObjectType({
+  name: "Calculator",
+  fields: {
+    add: {
+      type: new GraphQLNonNull(GraphQLInt),
+      args: {
+        right: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, { right }) => parent.add({ right }),
+    },
+    sub: {
+      type: new GraphQLNonNull(GraphQLInt),
+      args: {
+        right: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (parent, { right }) => parent.sub({ right }),
+    },
   },
-  format({ in_ }) {
-    return in_.trim();
+});
+
+const queryType = new GraphQLObjectType({
+  name: "Query",
+  fields: {
+    dummy: { type: new GraphQLNonNull(GraphQLString), resolve: () => "dummy!" },
+    format: {
+      type: new GraphQLNonNull(GraphQLString),
+      args: {
+        in_: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (_, { in_ }) => {
+        return in_.trim();
+      },
+    },
+    getCalculator: {
+      type: calculatorType,
+      args: {
+        left: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve: (_, { left }) => {
+        return new Calculator(left);
+      },
+    },
+    getNumVisitors: {
+      type: new GraphQLNonNull(GraphQLInt),
+      resolve: () => numVisitors,
+    },
   },
-  getCalculator({ left }) {
-    return new Calculator(left);
+});
+
+const mutationType = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    incrementNumVisitors: {
+      type: new GraphQLNonNull(GraphQLInt),
+      args: {
+        visitor: { type: visitorInputType },
+      },
+      resolve: (_, { visitor }) => {
+        if (!visitors.includes(visitor.id)) {
+          visitors.push(visitor.id);
+          numVisitors++;
+        }
+        return numVisitors;
+      },
+    },
   },
-  getNumVisitors() {
-    return numVisitors;
-  },
-  incrementNumVisitors({ visitor }) {
-    if (!visitors.includes(visitor.id)) {
-      visitors.push(visitor.id);
-      numVisitors++;
-    }
-    return numVisitors;
-  },
-};
+});
 
 const app = express();
+
 app.use(loggingMiddleware);
-app.all("/graphql", createHandler({ schema, rootValue }));
+
+const schema = new GraphQLSchema({
+  query: queryType,
+  mutation: mutationType,
+});
+
+app.all("/graphql", createHandler({ schema }));
 app.get("/", (_req, res) => {
   res.type("html");
   res.end(ruruHTML({ endopoint: "/graphql" }));
